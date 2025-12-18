8db79cfadd8285aab442cccf9f62f9eb0ec66b26
TMHuinh <huynhtruong260345@gmail.com>
2025-12-18 13:29:54 +0700

    crud-movie

--- src/main/java/org/example/booking_be/dto/responce/MovieResponse.java
+++ src/main/java/org/example/booking_be/dto/responce/MovieResponse.java
@@ -1,34 +1,19 @@
-package org.example.booking_be.entity;
+package org.example.booking_be.dto.responce;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.example.booking_be.enums.MovieStatus;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
 
-@Document(collection = "movies")
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class Movie {
-
-    @Id
+public class MovieResponse {
     private String id;
-
     private String title;
     private String description;
     private int duration;
     private LocalDate releaseDate;
-
     private String posterUrl;
     private String trailerUrl;
-
     private String genreId;
-
     private MovieStatus status;
 }
--- src/main/java/org/example/booking_be/reponsitory/MovieReponsitory.java
+++ src/main/java/org/example/booking_be/reponsitory/MovieReponsitory.java
@@ -1,9 +1,15 @@
 package org.example.booking_be.reponsitory;
 
 import org.example.booking_be.entity.Movie;
+import org.example.booking_be.enums.MovieStatus;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieReponsitory extends MongoRepository<Movie, String> {
+    List<Movie> findByStatus(MovieStatus status);
+    List<Movie> findByTitleContainingIgnoreCase(String title);
+    List<Movie> findByGenreId(String genreId);
 }
