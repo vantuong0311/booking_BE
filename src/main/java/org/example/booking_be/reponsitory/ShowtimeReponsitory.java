dc1e651161195fe222b4ecaebc321b7b8a0052b6
TMHuinh <huynhtruong260345@gmail.com>
2026-01-13 11:09:20 +0700

    update showtime getAllByMovieId

--- src/main/java/org/example/booking_be/reponsitory/ShowtimeReponsitory.java
+++ src/main/java/org/example/booking_be/reponsitory/ShowtimeReponsitory.java
@@ -4,7 +4,9 @@ import org.example.booking_be.entity.Showtime;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ShowtimeReponsitory extends MongoRepository<Showtime,String> {
-
+    List<Showtime> findByMovieId(String movieId);
 }
